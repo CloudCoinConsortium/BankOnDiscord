0423632b8d4ff7031fe24c58968aa82d0c0147e3
Navraj Singh <navraj@outlook.com>
2022-12-29 10:25:17 +1100

    Text changes

--- help.py
+++ help.py
@@ -10,7 +10,7 @@ async def Help():
     return mainContent + depositContent + withdrawContent + moveContent + payContent + balanceContent + statementContent + deletewalletContent
 
 async def MainHelp():
-    return '**✳️ WELCOME TO CLOUDCOIN VAULT ✳️**\nThis bot allows you to deposit, withdraw and transfer, and pay out CloudCoins. This software is provided free of charge with all bugs, defects and vulnerabilities. \n\n**BASIC COMMANDS**'
+    return '**✳️ WELCOME TO CLOUDCOIN VAULT ✳️**\nThis bot allows you to deposit, withdraw, transfer, and pay out CloudCoins. This software is provided free of charge with all bugs, defects and vulnerabilities. \n\n**BASIC COMMANDS**'
 
 async def MainNFTHelp():
     return '**✳️ WELCOME TO CLOUDCOIN NFT VAULT ✳️**\nThis bot allows you to create, list and withdraw NFTs powered by CloudCoin. This software is provided free of charge with all bugs, defects and vulnerabilities included free from the CloudCoin Consoritum. \n\n**BASIC COMMANDS ➡️**'
@@ -22,25 +22,25 @@ async def balanceHelp():
     return '\n\n**🔎 BALANCE**\n`/bank balance` Returns the number of coins in  your Coin Bank.\nNo extra information is required.'
 
 async def deletewalletHelp():
-    return '\n\n**🔎 DELETE WALLET **\n`/bank deletebank` Deletes your coin bank if it\'s empty.\nPlease withdraw all your coins before issuing this command.'
+    return '\n\n**🔎 DELETE COIN BANK **\n`/bank deletebank` Deletes your Coin Bank if it\'s empty.\nPlease withdraw all your coins before issuing this command.'
 
 async def payHelp():
-    return "\n\n**❤️ PAY**\n`/bank pay` Places money from your account into the bot's account and tells the bot about the payment.\nRequires the number of coins to give the bot: `/bank pay 50` where 50 is the number of coins to give the bot. "
+    return "\n\n**❤️ PAY**\n`/bank pay` Places money from your Coin Bank into the bot's Coin Bank and tells the bot about the payment.\nRequires the number of coins to give the bot: `/bank pay 50` where 50 is the number of coins to give the bot. "
     
 async def moveHelp():
-    return '\n\n**↔️ TRANSFER**\n`/bank transfer` Transfers coins from your account to the account of another person.\nRequires the number of coins to transfer and the name of the user that will receive them: `/bank transfer 10 larryG#3345` where 10 is the number of coins to transfer and larryG#3345 is the Discord user to receive the coins. They must have a Coin Bank on this bot to receive coins. '
+    return '\n\n**↔️ TRANSFER**\n`/bank transfer` Transfers coins from your Coin Bank to the Coin Bank of another person.\nRequires the number of coins to transfer and the name of the user that will receive them: `/bank transfer 10 larryG#3345` where 10 is the number of coins to transfer and larryG#3345 is the Discord user to receive the coins. They must have a Coin Bank on this bot to receive coins. '
 
 async def WithdrawHelp():
     return '\n\n**📤 WITHDRAW**\n`/bank withdraw` Removes coins from your Coin Bank.\nRequires the amount of coins to be removed: `/bank withdraw 33` where 33 is the number of coins to be removed.\nClick image. Open original and save the file.'
 
 async def DeleteWalletHelp():
-    return '\n\n** 🚫 DELETE COIN BANK **\nDeletes the users coin bank.\n\tRequest:\n /deletebank\nReturns:\n\tWallet Deleted\nor\n\tYour Wallet Must Be Empty to be Deleted. Withdraw your Coins first.'
+    return '\n\n** 🚫 DELETE COIN BANK **\nDeletes the users Coin Bank.\n\tRequest:\n /deletebank\nReturns:\n\tWallet Deleted\nor\n\tYour Wallet Must Be Empty to be Deleted. Withdraw your Coins first.'
 
 async def ShowCoinsHelp():
     return '\n\n** 👀 SHOWCOINS **\nShows all the coins that are in the users wallet. This number includes coins in the Bank folder and in the Fracked folder.\n\tRequest:\n `/showcoins`\nReturns an integer something like:\n\t33'
 
 async def DepositHelp():
-    return '\n\n** 📥 DEPOSIT**\n`/bank deposit` Creates a Coin Bank if one does not exist. Uploads a coin file into your account.\n Files must have a .bin or .png file extension.  To upload coins to your account, first type /bank deposit. Before you submit this command, click the “+” icon to the left of the text box and select a coin file to upload. Once your file is uploaded, submit the command.'
+    return '\n\n** 📥 DEPOSIT**\n`/bank deposit` Creates a Coin Bank if one does not exist. Uploads a coin file into your Coin Bank.\n Files must have a .bin or .png file extension.  To upload coins to your Coin Bank, first type /bank deposit. Before you submit this command, click the “+” icon to the left of the text box and select a coin file to upload. Once your file is uploaded, submit the command.'
 
 async def NFTCreateHelp():
     return '\n\n** 🎨 CREATE**\n`/nft create title description\nThis bot allows you to create NFTs from your cloudcoins in CloudBank. You must have a non zero balance in your wallet to create an NFT. This will create a new NFT wallet for you on the server.\n\n '
