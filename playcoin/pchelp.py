b1c2be00aa76d226e1c6697b42d1e3a3b3653641
Navraj Singh <navraj@outlook.com>
2023-05-12 19:03:00 +1000

    Changed Help

--- playcoin/pchelp.py
+++ playcoin/pchelp.py
@@ -5,7 +5,8 @@ async def Help():
     lockContent = await lockHelp()
     depositContent = await depositHelp()
     myWalletContent = await MyWalletHelp()
-    return mainContent  + balanceContent + lockContent + depositContent + transferContent + myWalletContent
+    payContent = await payHelp()
+    return mainContent  + balanceContent + lockContent + depositContent + transferContent + payContent + myWalletContent
 
 async def MainHelp():
     return '**✳️ WELCOME TO PLAYCOIN BOT ✳️**\nThis bot allows you to Remove or put your Playcoins in a locker. This software is provided free of charge with all bugs, defects and vulnerabilities. \n\n**BASIC COMMANDS**'
@@ -15,6 +16,9 @@ async def depositHelp():
 
 async def transferHelp():
     return "\n\n** ⮂ TRANSFER ** \n Moves coins from your account to another Discord users account.\nExample:\n/transfer <amount> <discord user>\nsuch as:\n/transfer 10.33 JohnDoe#0982Note, the reciever will need to install the PCBOT before they can get their money. "
+
+async def payHelp():
+    return "\n\n** ⮂ PAY ** \n Moves coins from your account to Owner's Discord account.\nExample:\n/pay <amount> \nsuch as:\n/pay 10.33 "
 
 async def lockHelp():
     return '\n\n**🧾 WITHDRAW**\n`/withdraw <amount>`\nRemoves coins from your wallet and returns a code that you can give to your friends. Specify the amount of coins you want to withdraw in the command. Sample:\n/withdraw 99.0921'
