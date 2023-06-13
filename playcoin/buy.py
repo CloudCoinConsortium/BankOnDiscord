6764835cdd96db746e6915bb7852172f055bd450
Navraj Singh <navraj@outlook.com>
2023-06-13 20:43:59 +1000

    Created order id with qty and price

--- playcoin/buy.py
+++ playcoin/buy.py
@@ -4,21 +4,21 @@ import json
 from constants import pcbaseUrl, pay_url
 # shows total coins in the user balance
 
-async def Buy(wallet, event: hikari.DMMessageCreateEvent):
+async def Buy(wallet, event: hikari.DMMessageCreateEvent, qty, price, seller):
     wallet = wallet.replace("#","%23")
 
     url = "http://localhost:3000/api/order"
-
+    total_amount = str(qty * price)
     payload = json.dumps([
   {
     "reference_id": "PUHF",
     "amount": {
       "currency_code": "USD",
-      "value": "175.00",
+      "value": total_amount,
       "breakdown": {
         "item_total": {
           "currency_code": "USD",
-          "value": "175.00"
+          "value": total_amount
         },
         "shipping": {
           "currency_code": "USD",
@@ -28,28 +28,31 @@ async def Buy(wallet, event: hikari.DMMessageCreateEvent):
     },
     "items": [
       {
-        "name": "Enterprise Subscription",
-        "description": "Enterprise Subscription: 1 year",
+        "name": "Playcoins",
+        "description": "Playcoins",
         "sku": "sku01",
         "unit_amount": {
           "currency_code": "USD",
-          "value": "175.00"
+          "value": str(price)
         },
-        "quantity": "1",
+        "quantity": str(qty),
         "category": "DIGITAL_GOODS"
       }
     ]
   }
 ])
+    print(payload)
     headers = {
     'Content-Type': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
     responseJson = response.json()
-    print(response.text)
-    print(json.dumps(response.json()))
+    #print(response.text)
+    #print(json.dumps(response.json()))
     if responseJson['status'] == "CREATED":
+        orderId = responseJson['id']
+        
         print('Order created:' + responseJson['id'])
         url = pay_url + '?orderID=' + responseJson['id']
         await event.message.respond("Please make a payment to :\n" + url)
