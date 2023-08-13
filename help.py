ef344a65414109c0fadfb21d05fb8678cb4caf83
Sean Worthington <sean@worthington.net>
2023-08-12 21:24:10 -0700

    Update help.py

--- help.py
+++ help.py
@@ -10,37 +10,56 @@ async def Help():
     return mainContent + depositContent + withdrawContent + moveContent + payContent + balanceContent + statementContent + deletewalletContent
 
 async def MainHelp():
-    return '**✳️ WELCOME TO CLOUDCOIN VAULT ✳️**\nThis bot allows you to deposit, withdraw, transfer, and pay out CloudCoins. This software is provided free of charge with all bugs, defects and vulnerabilities. \n\n**BASIC COMMANDS**'
+     return '**✳️ WELCOME TO COIN BOT ✳️**\nThis bot allows you to deposit, withdraw, transfer, and pay out cloud currencies using folders on a shared server. This software is provided free of charge with all bugs, defects and vulnerabilities by the CloudCoin Consortium.\n\nThis bot stores your coins on a remote server where they can be accessed from any device using chat programs such as Discord or Telegram. It is important to understand that the remote server does not have data supremacy and coins can be lost and hacked. Please do not store large amounts of coins here.\n\n**BASIC COMMANDS**'
 
 async def MainNFTHelp():
     return '**✳️ WELCOME TO CLOUDCOIN NFT VAULT ✳️**\nThis bot allows you to create, list and withdraw NFTs powered by CloudCoin. This software is provided free of charge with all bugs, defects and vulnerabilities included free from the CloudCoin Consoritum. \n\n**BASIC COMMANDS ➡️**'
 
 async def statementHelp():
-    return '\n\n**🧾 STATEMENT**\n`/bank statement <page>` Returns records of transactions as a set of 10 records.\ne.g. /bank statement 1 returns first page of statement'
+    return '\n\n**🧾 STATEMENT**\n`/statement <page>` Returns records of transactions as a set of 100 records.\ne.g. /statement 1 returns first page of your latest transaction records'
 
 async def balanceHelp():
-    return '\n\n**🔎 BALANCE**\n`/bank balance` Returns the number of coins in  your Coin Bank.\nNo extra information is required.'
+    return '\n\n**🔎 BALANCE**\n`/balance` Returns the number of coins in  your folder.'
 
 async def deletewalletHelp():
-    return '\n\n**🔎 DELETE COIN BANK **\n`/bank deletebank` Deletes your Coin Bank if it\'s empty.\nPlease withdraw all your coins before issuing this command.'
+    return '\n\n**🗑️ DELETE FOLDER **\n`/deletefolder` Deletes your folder if it\'s empty.\nPlease withdraw all your coins before issuing this command.'
 
 async def payHelp():
-    return "\n\n**❤️ PAY**\n`/bank pay` Places money from your Coin Bank into the bot's Coin Bank and tells the bot about the payment.\nRequires the number of coins to give the bot: `/bank pay 50` where 50 is the number of coins to give the bot. "
+     return "\n\n**❤️ PAY**\n`/pay <amount> <description>` Moves coins from your folder into the bot's folder and tells the bot about the payment.\nRequires the number of coins to give to the bot: `/pay 50` where 50 is the number of coins to give the bot. Unless you are specifically trying to pay the bot, use transfer instead."
     
 async def moveHelp():
-    return '\n\n**↔️ TRANSFER**\n`/bank transfer` Transfers coins from your Coin Bank to the Coin Bank of another person.\nRequires the number of coins to transfer and the name of the user that will receive them: `/bank transfer 10 larryG#3345` where 10 is the number of coins to transfer and larryG#3345 is the Discord user to receive the coins. They must have a Coin Bank on this bot to receive coins. '
+   return '\n\n**↔️ TRANSFER**\n`/transfer <amount> <username> <description>` Transfers coins from your folder to the folder of another person.\nRequires the number of coins to transfer, the name of the user that will receive them and an optional description. The description maybe required if the receiver needs to know who you are.  If you are sending coins to another chat program, like from Discord to Telegram, you will need to put an @telegram or @discord at the end of the username. : `/transfer 10 larryG#3345@discord For purchase 28837` where 10 is the number of coins to transfer and larryG#3345 is user to receive the coins. If there is no folder yet for the receiver, the receiver must include the bot and issue the command: new. A folder will be created for that person.'
 
 async def WithdrawHelp():
-    return '\n\n**📤 WITHDRAW**\n`/bank withdraw` Removes coins from your Coin Bank.\nRequires the amount of coins to be removed: `/bank withdraw 33` where 33 is the number of coins to be removed.\nClick image. Open original and save the file.'
+    return '\n\n**📤 WITHDRAW CODE**\n`/withdraw <amount> <description>` Removes coins from your folder and puts them in a locker on the RAIDA. Returns the locker key that you can give to others.\nRequires the amount of coins to be removed: `/withdraw 33` where 33 is the number of coins to be removed. Description is optional'
 
-async def DeleteWalletHelp():
-    return '\n\n** 🚫 DELETE COIN BANK **\nDeletes the users Coin Bank.\n\tRequest:\n /deletebank\nReturns:\n\tWallet Deleted\nor\n\tYour Wallet Must Be Empty to be Deleted. Withdraw your Coins first.'
+async def WithdrawFileHelp():
+    return '\n\n**📤 WITHDRAW FILE**\n`/withdrawfile <amount> <description>` Removes coins from your folder and returns them as a file. You can give this file to others.\nRequires the amount of coins to be removed: `/withdraw 33` where 33 is the number of coins to be removed. Description is optional'
 
-async def ShowCoinsHelp():
-    return '\n\n** 👀 SHOWCOINS **\nShows all the coins that are in the users wallet. This number includes coins in the Bank folder and in the Fracked folder.\n\tRequest:\n `/showcoins`\nReturns an integer something like:\n\t33'
 
 async def DepositHelp():
-    return '\n\n** 📥 DEPOSIT**\n`/bank deposit` Creates a Coin Bank if one does not exist. Uploads a coin file into your Coin Bank.\n Files must have a .bin or .png file extension.  To upload coins to your Coin Bank, first type /bank deposit. Before you submit this command, click the “+” icon to the left of the text box and select a coin file to upload. Once your file is uploaded, submit the command.'
+    return '\n\n** 📥 DEPOSIT CODE**\n`/deposit <description>` Creates a folder if one does not exist. Takes the locker number you provided to the RAIDA and downloads any coins there to your folder.\n Includes the description (optional) in your statements.'
+
+async def DepositFileHelp():
+    return '\n\n** 📥 DEPOSIT FILE**\n`/depositfile <description>` Creates a folder if one does not exist. To upload coins to your folderk, first type /depositfile. Before you submit this command, click the “+” icon to the left of the text box and select a coin file to upload. Once your file is uploaded, submit the command. only files with .bin, .stack and .png are allowed. Description is optional.'
+
+async def AddAPI():
+     return '\n\n** 🔑 ADD SALES API**\n`/api <PayPal api key>` Allows you to upload an API Key for PayPal that allows a coin buyer to put dollars into your PayPal account. \n`api M7zJ4i4NVDNLid7drBGEFWxSb8MnVonawWuu5JGr6AHVJkK6-HF6aqx_K630tRCyF8G2dkjlz1-GKvyV`\nThis is required to sell your coins. Instructions for creating an API key are here: https://www.paypal.com/us/cshelp/article/how-do-i-create-rest-api-credentials-ts1949'
+async def SellCoins():
+     return '\n\n** 💸 SELL COINS**\n`/sell <amount> <price>` Marks the amount of coins you specified for sale at the price you specified. \n`/sell 25 .03` puts 25 coins up for sale at $.03 each.'
+
+async def BuyCoins():
+     return '\n\n** ❤️ BUY COINS**\n`/buy <amount> <price>` Attempts to buy the number of coins at the price you specified. Requires that there are enough coins for sale at that price.  \n`/buy 20 .03` will give you a link to purchase them with dollars. The link is only good for a few minutes. After you make the purchase, check your balance or statement to see if it worked.'
+
+async def CoinExchanges():
+     return '\n\n** 👀 SHOW EXCHANGES**\n`/exchanges` Shows the past exchanges and their prices and dates. Note, it is a common practice for sellers to create fake transactions that are at higher prices in order to fool buyers into paying too much thinking the price is higher than it is. Buyer beware.'
+
+async def DeleteAIP():
+    return '\n\n** 🗑🚫 DELETE API **\nDeletes your API Key.\n\tRequest:\n `/deleteapi`\nReturns success statement. Sales will become impossible.'
+
+
+async def ShowCoinsHelp():
+    return '\n\n** 👀 SHOWCOINS **\nShows the serial numbers of all the coins in your folder. Only used for NFTs.\n\tRequest:\n `/showcoins`\nReturns a list of serial numbers.'
 
 async def NFTCreateHelp():
     return '\n\n** 🎨 CREATE**\n`/nft create title description\nThis bot allows you to create NFTs from your cloudcoins in CloudBank. You must have a non zero balance in your wallet to create an NFT. This will create a new NFT wallet for you on the server.\n\n '
